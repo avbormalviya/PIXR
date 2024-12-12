cbbc155edc7b3c30e45ac8e5b3ab390b8be73ef9
Vimal Borana <112873944+avbormalviya@users.noreply.github.com>
2024-12-12 19:51:10 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -9,18 +9,9 @@ const app = express();
 initSocket(app);
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://pixr-ten.vercel.ap",
     credentials: true
 }))
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "https://pixr-ten.vercel.app");  // Frontend URL
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
 
 app.use(express.json({
     limit: "10mb"
@@ -34,8 +25,6 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 
 app.use(cookieParser())
-
-app.options("*", cors());
 
 
 import userRoutes from "./routes/user.routes.js";
