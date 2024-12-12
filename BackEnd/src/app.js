85a9f41959efddccfec6bd6a461cff906e6dd5fb
Vimal <avbormalviya2006@gmail.com>
2024-12-12 20:58:44 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -9,13 +9,11 @@ const app = express();
 initSocket(app);
 
 app.use(cors({
-<<<<<<< HEAD
     origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
-=======
-    origin: process.env.CORS_ORIGIN,
->>>>>>> cf22836cd4c3c405673584a57845a65d95b99442
     credentials: true
 }))
+
+app.options('*', cors());  // Handle preflight requests
 
 app.use(express.json({
     limit: "10mb"
