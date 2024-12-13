fd4fdc51233303ae8dc3ce494d2854df1bd53e65
Vimal Borana <112873944+avbormalviya@users.noreply.github.com>
2024-12-13 11:47:52 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 initSocket(app);
 
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
