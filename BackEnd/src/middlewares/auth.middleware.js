e1ea033e5fe763d3fb13aebccb23b3d14a399286
Vimal <avbormalviya2006@gmail.com>
2025-04-20 21:29:54 +0530

    chore: Remove unnecessary blank line in auth middleware

--- BackEnd/src/middlewares/auth.middleware.js
+++ BackEnd/src/middlewares/auth.middleware.js
@@ -3,7 +3,6 @@ import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
