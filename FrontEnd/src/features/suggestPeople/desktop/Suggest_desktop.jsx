e4028788bd9b3399d7e6ca6e4293c1d2d588f06b
Vimal <avbormalviya2006@gmail.com>
2025-07-25 22:34:48 +0530

    feat: add loading skeleton for suggested users while fetching data in Suggest_desktop component

--- FrontEnd/src/features/suggestPeople/desktop/Suggest_desktop.jsx
+++ FrontEnd/src/features/suggestPeople/desktop/Suggest_desktop.jsx
@@ -6,14 +6,21 @@ import { FollowUserCard, NormalUserCard } from "../../../components/userCard/Use
 import { useSelector } from "react-redux"
 import { fetchAndSetSuggestedUsers } from "../../../utils/getSuggestedUsers"
 
-import { useEffect } from "react"
+import Skeleton from '@mui/material/Skeleton';
+
+import { useEffect, useState } from "react"
 
 export const Suggest_desktop = () => {
 
     const { user,suggestedUsers } = useSelector(state => state.user);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        fetchAndSetSuggestedUsers();
+        (async () => {
+            setLoading(true);
+            await fetchAndSetSuggestedUsers();
+            setLoading(false);
+        })();
     })
 
     return (
@@ -23,9 +30,17 @@ export const Suggest_desktop = () => {
             <section className={style.suggest}>
                 <h1>Suggested for you</h1>
 
+                { loading &&
+                    <div className={style.suggested_user_loading_skeleton}>
+                        <Skeleton variant="circular" width={40} height={40} sx={{ bgcolor: "var(--background-ternary)" }} />
+                        <Skeleton variant="text" width="60%" height={20} sx={{ bgcolor: "var(--background-ternary)" }} />
+                        <Skeleton variant="text" width="40%" height={20} sx={{ bgcolor: "var(--background-ternary)" }} />
+                    </div>
+                }
+
                 {
                     suggestedUsers?.map((suggestedUser, index) => (
-                        <FollowUserCard key={ index } fullName={ suggestedUser.fullName } userName={ suggestedUser.userName } profilePic={ suggestedUser.profilePic } isFollower={ suggestedUser.isFollower } isRing={ suggestedUser.hasStories } />    
+                        <FollowUserCard key={ index } fullName={ suggestedUser.fullName } userName={ suggestedUser.userName } profilePic={ suggestedUser.profilePic } isFollower={ suggestedUser.isFollower } isRing={ suggestedUser.hasStories } />
                     ))
                 }
             </section>
